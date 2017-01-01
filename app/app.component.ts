8402af657d488cd70facb60dc1e8a9d63c658abb
Matthew Will <mattwill09@gmail.com>
2017-01-01 08:47:39 -0600

    initial bootstrapping work

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'app',
+  template: '<h1>My App</h1>'
 })
-export class AppComponent  { name = 'Angular-2'; }
+
+export class AppComponent {}
